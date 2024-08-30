13c7721963003f58c9ab02cf43813de3dbf8fe7a
David Cannan <davidacannan@gmail.com>
2024-08-30 15:48:21 -0400

    Added web/build directory for embedding

---
+++ web/build/static/js/main.cc990a47.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpregistry-service-frontend"]=this["webpackJsonpregistry-service-frontend"]||[]).push([[0],[function(e,s){}],[[0,1]]]);
+//# sourceMappingURL=main.cc990a47.chunk.js.map
